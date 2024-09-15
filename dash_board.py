9010962a72962f5576f422eaa467f27788fab7ca
Ahmed Mohamed <131635923+Ahmedmohamed998@users.noreply.github.com>
2024-09-15 21:13:39 +0300

    Update dash_board.py

--- dash_board.py
+++ dash_board.py
@@ -9,12 +9,13 @@ st.markdown("")
 st.markdown("")
 @st.cache_data
 def df_get_from_csv():
-    df = pd.read_csv(r"Bank.csv")
+    df = pd.read_csv("Bank churn.csv")
     return df
 
 df = df_get_from_csv()
-df.drop_duplicates(inplace=True)
+df.drop(['Unnamed: 0'],axis=1,inplace=True)
 df['Exited'] = [1 if i > 0.5 else 0 for i in df['Exited']]
+df.drop_duplicates(inplace=True)
 df['joining_year'] = 2024 - df['Tenure']
 
 # Streamlit app
@@ -46,7 +47,7 @@ with col1:
     fig = px.pie(df, names='Gender', title="Gender Distribution", color_discrete_sequence=['blue', 'pink'])
     st.plotly_chart(fig)
     with st.expander("Insights"):
-        st.write("""- Approximately 56.4% of the customers are male.""")
+        st.write("""- Approximately 58% of the customers are male.""")
 
 # Age Distribution Histogram
 with col2:
@@ -55,13 +56,13 @@ with col2:
     fig.update_traces(marker=dict(line=dict(color='blue', width=2)))
     st.plotly_chart(fig)
     with st.expander("Insights"):
-        st.write("""- The average age of customers is around 38.13 years.""")
+        st.write("""- The average age of customers is around 36.23 years.""")
 
 # Churned Customers by Age Group
 with col3:
     st.subheader("Churned Customers by Age Group")
-    age_bins = [18, 30, 40, 50, 60, 70, 80]
-    age_labels = ['18-30', '31-40', '41-50', '51-60', '61-70', '71-80']
+    age_bins = [18, 30, 40, 50, 54]
+    age_labels = ['18-30', '31-40', '41-50', '51-53']
     df['AgeGroup'] = pd.cut(df['Age'], bins=age_bins, labels=age_labels, right=False)
     churn_count = df[df['Exited'] == 1].groupby('AgeGroup')['Exited'].count().reset_index()
     fig = px.bar(churn_count, x='AgeGroup', y='Exited', title='Number of Churned Customers by Age Group', labels={'Exited': 'Number of Churned Customers', 'AgeGroup': 'Age Group'}, height=450, color_discrete_sequence=['red'])
@@ -87,7 +88,7 @@ with col2:
     fig = px.bar(credit, y='Count', x='Status', title='Has Credit Card Distribution', color_discrete_sequence=['red', 'lightred'])
     st.plotly_chart(fig)
     with st.expander("Insights"):
-        st.write("""- A significant proportion (around 75.4%) of customers have a credit card (HasCrCard).""")
+        st.write("""- A significant proportion (around 76.7%) of customers have a credit card (HasCrCard).""")
 
 # Credit Score Distribution Histogram
 with col3:
@@ -96,7 +97,7 @@ with col3:
     fig.update_traces(marker=dict(line=dict(color='blue', width=2)))
     st.plotly_chart(fig)
     with st.expander("Insights"):
-        st.write("""- The average credit score is approximately 656.45. - The minimum credit score is 350, and the maximum is 850.""")
+        st.write("""- The average credit score is approximately 656.12 - The minimum credit score is 526, and the maximum is 792.""")
 
 # Exited Customers Bar Chart
 with col1:
@@ -106,7 +107,7 @@ with col1:
     fig = px.bar(Exited, y='Count', x='Status', title='Exited Customers Distribution', color_discrete_sequence=['blue'])
     st.plotly_chart(fig)
     with st.expander("Insights"):
-        st.write("""- About 12.7% of customers have exited the bank (Exited = 1). - The majority (about 87.3%) have not exited the bank (Exited = 0).""")
+        st.write("""- About 11.2% of customers have exited the bank (Exited = 1). - The majority (about 88.7%) have not exited the bank (Exited = 0).""")
 
 # Joining Year Line Chart
 with col2:
@@ -118,7 +119,7 @@ with col2:
     fig = px.line(year, x='Joining Year', y='Count', title="Joining Year Distribution", color_discrete_sequence=['red'])
     st.plotly_chart(fig)
     with st.expander("Insights"):
-        st.write("""- The year 2022 saw the highest number of customers, totaling 30.331k. - Conversely, 2024 had the lowest number of customers, with only 8431. - On average, we attract around 27.500k customers each year.""")
+        st.write("""- The year 2022 saw the highest number of customers, totaling 15.368k. - Conversely, 2024 had the lowest number of customers, with only 3884. - On average, we attract around 13.800k customers each year.""")
 
 # Active Member Distribution Pie Chart
 with col3:
@@ -126,7 +127,7 @@ with col3:
     fig = px.pie(df, names='IsActiveMember', title="Active Member Distribution", color_discrete_sequence=['red', 'black'])
     st.plotly_chart(fig)
     with st.expander("Insights"):
-        st.write("""- Approximately 50.3% of customers are active members (IsActiveMember).""")
+        st.write("""- Approximately 50.6% of customers are active members (IsActiveMember).""")
 
 # Plot: Churned Customers by Country
 st.markdown("### Churned Customers by Country")
